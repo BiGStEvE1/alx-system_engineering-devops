47add872b7829b59ed077ddafac56503d2616073
BiGStEvE1 <haimoudmustapha@gmail.com>
2024-02-05 07:47:57 -0800

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,15 @@
-# This manuscript increases the amount of traffic an Nginx server can handle
-
-# Increase the ULIMIT of the default file
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
+# fix a Nginx server that was getting many failed requests
+exec {'increase ulimit':
+  path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+  command => 'sed -i "s/-n 15/-n 30000/g" /etc/default/nginx'
 }
 
-# Restart Nginx
--> exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+exec {'add equal sign to assign value':
+  path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+  command => 'sed -i "s/ulimit $ULIMIT/ulimit=$ULIMIT/g" /etc/init.d/nginx'
+}
+
+exec {'reload and restart Nginx':
+  path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+  command => 'service nginx reload; service nginx restart'
 }
